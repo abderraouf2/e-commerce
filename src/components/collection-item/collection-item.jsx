a0a0a4fd82417d71c5691ad878ed1e9b3668f67c
abderraouf2 <79739572+abderraouf2@users.noreply.github.com>
2022-02-10 12:23:12 +0100

    add shopping data

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import Costumbutton from '../costumbutton/costumbutton';
+import {AddItem} from '../../Redux/cart/cart.action'
 import './collection-item.scss';
 
-const  collectionItem= ( {id,price,imageUrl,name} )=> {
+const  collectionItem= ( {item,AddItem} )=> {
+  const {price,imageUrl,name} = item;
   return (
     <div className='collection-item'>
         <div className='image'
@@ -13,9 +17,14 @@ const  collectionItem= ( {id,price,imageUrl,name} )=> {
             <span className='name'>{ name }</span> 
             <span className='price'>{ price }</span> 
           </div>
-          
+          <Costumbutton inverted onClick={()=> AddItem(item)}>Add to Cart</Costumbutton>
     </div>
   )
 }
 
-export default collectionItem;
+const mapDispatchToProps=dispatch=>({
+  AddItem: item=>dispatch(AddItem(item))
+})
+
+
+export default connect(null,mapDispatchToProps)(collectionItem);
--- src/components/costumbutton/costumbutton.jsx
+++ src/components/costumbutton/costumbutton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './costumbutton.scss'
 
- const costumbutton=({children, isGoogleSignIn, ...otherProps})=> {
+ const costumbutton=({children, isGoogleSignIn,inverted, ...otherProps})=> {
   return (
     <button 
-    className={`${ isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
+    className={`${ inverted ? 'inverted' : ''} ${ isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
               {...otherProps}>
       {children}
     </button>
